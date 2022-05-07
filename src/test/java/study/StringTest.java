70448a366b4bc424791decb63c0fc41fed5df9a8
isemang <a55306039@gmail.com>
2022-05-07 12:34:35 +0900

    test - 학습테스트 실습 요구사항 1 테스트코드 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,29 @@
 package study;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+//String 클래스에 대한 학습 테스트
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    @DisplayName("1,2를 ,로 split했을 때, 1과 2로 잘 분리되는지 확인")
+    public void split_1_and_2_by_comma() {
+        String str = "1,2";
+        String regex = ",";
+
+        String[] arr = str.split(regex);
+        assertThat(arr).contains("1", "2");
+    }
+
+
 }
