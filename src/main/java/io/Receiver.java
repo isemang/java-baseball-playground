31301de7a09da4528a622ae0299f6dc3ca4bd5ef
isemang <a55306039@gmail.com>
2022-05-09 13:23:47 +0900

    feat - 숫자야구 게임 기능 추가(Receiver, RecievedValue) - 입력받을 스캐너를 생성하는 Receiver 클래스 - 입력받은 숫자를 저장하는 ReceivedValue 클래스 생성

---
+++ src/main/java/io/Receiver.java
@@ -0,0 +1,26 @@
+package io;
+
+import java.util.Scanner;
+
+/**
+ * 입력받을 스캐너를 생성한다.
+ * 숫자를 입력해 주세요: 프린트한다
+ * 스캐너로 입력받은 숫자를 revievedValue에 저장한다
+ * revievedValue를 judge에 던진다
+ */
+public class Receiver {
+    Scanner sc;
+    private RecievedValue recievedValue;
+
+    public Receiver() {
+        Printer.printInputNum();
+
+        sc = new Scanner(System.in);
+
+        recievedValue = new RecievedValue(Integer.parseInt(sc.nextLine()));
+    }
+
+    public RecievedValue getRecievedValue() {
+        return recievedValue;
+    }
+}
---
+++ src/main/java/io/RecievedValue.java
@@ -0,0 +1,12 @@
+package io;
+
+/**
+ * 입력받은 숫자를 가지고 있는 클래스
+ */
+public class RecievedValue {
+    private int value;
+
+    public RecievedValue(int value) {
+        this.value = value;
+    }
+}
