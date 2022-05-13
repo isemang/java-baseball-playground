4c71d26c1a561187a85728a777ac625d58388751
isemang <a55306039@gmail.com>
2022-05-13 10:49:54 +0900

    feat - 메인 메서드 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+import io.Printer;
+import io.Receiver;
+import judge.Judge;
+import judge.StrikeBallValue;
+import generator.NumberGenerator;
+
 public class Main {
     public static void main(String[] args) {
+        NumberGenerator generator = new NumberGenerator();
+        int randomValue = generator.getGeneratedValue().getValue();
+        int inputValue = 0;
 
+        while (randomValue != inputValue) {
+            Receiver receiver = new Receiver();
+            inputValue = receiver.getRecievedValue().getValue();
+
+            Judge.judgeStrikeAndBall(inputValue, randomValue);
+
+            StrikeBallValue judgeResult = Judge.getJudgeResult();
+
+            Printer.printJudgeResult(judgeResult);
+        }
+
+        Printer.printGameEnd();
     }
 }
