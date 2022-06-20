ce10b2b7bb2f8e44fb0602cbdb033aeb7c0aff26
isemang <a55306039@gmail.com>
2022-06-20 19:11:22 +0900

    refactor - 프린트 상수 enum class로 분리

---
+++ src/main/java/io/PrintConstant.java
@@ -0,0 +1,14 @@
+package io;
+
+public enum PrintConstant {
+    INPUT_NUM("숫자를 입력해 주세요: "),
+    STRIKE_STRING("스트라이크 "),
+    BALL_STRING("볼 "),
+    GAME_END("개의 숫자를 모두 맞히셨습니다! 게임 종료\n 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+    private final String str;
+
+    PrintConstant(String str) {
+        this.str = str;
+    }
+}
--- src/main/java/io/Printer.java
+++ src/main/java/io/Printer.java
@@ -4,16 +4,14 @@ import judge.StrikeBallValue;
 import judge.Judge;
 import proceedGame.NumberGenerator;
 
+import static io.PrintConstant.*;
+
 /**
  * 숫자 입력해달라는 말 출력
  * judge의 결과값을 x스트라이크 x볼이라고 그대로 출력
  * 게임 종료 시 게임 종료 문구 출력
  */
 public class Printer {
-    private static final String INPUT_NUM = "숫자를 입력해 주세요: ";
-    private static final String STRIKE_STRING = "스트라이크 ";
-    private static final String BALL_STRING = "볼 ";
-    private static final String GAME_END = "개의 숫자를 모두 맞히셨습니다! 게임 종료\n 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     private Printer(){
     }
@@ -24,7 +22,7 @@ public class Printer {
 
     public static void printJudgeResult() {
         StrikeBallValue judgeResult = Judge.getJudgeResult();
-        System.out.println(judgeResult.getBall()+BALL_STRING+judgeResult.getStrike()+STRIKE_STRING);
+        System.out.println(judgeResult.getBall() + "" + BALL_STRING + judgeResult.getStrike() + STRIKE_STRING);
     }
 
     public static void printGameEnd() {
